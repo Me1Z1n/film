d3d6bb05c73e6748e875fd9c7364248700275abc
MeiZin <bazantetyana111@gmail.com>
2020-03-11 17:57:29 +0200

    Revert "в"
    This reverts commit faf52a0578accc552b8c0963b32319e4a58c318e.

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ fetch('https://api.themoviedb.org/3/trending/movie/week?api_key=3a66dca4ab982982
   .then(response => response.json())
   .then(res => {
     const root = document.createElement('div')
-    document.body.appendChild(root)
-
-    res.results.map(el => {
-      const card = document.createElement('div')
 
       const title = document.createElement('p1')
       title.innerText = el.title
