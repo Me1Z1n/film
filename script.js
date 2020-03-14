60f5d1a0accaa87bdfcffb93d94f0b337a5d0381
MeiZin <bazantetyana111@gmail.com>
2020-03-14 18:35:49 +0200

    працює перехід між сторінками

--- script.js
+++ script.js
@@ -1,31 +1,22 @@
-function GetTrendFilms(){
+﻿function GetTrendFilms(root){
 	fetch('https://api.themoviedb.org/3/trending/movie/week?api_key=3a66dca4ab982982a2e4d01db94410b4&language=uk')
-    .then(response => response.json())
+	.then(response => response.json())
     .then(res => {
-	const root = document.createElement('div')
-    document.body.appendChild(root)
-
-	res.results.map(el => {
-	const card = CreateFilmCard(el)
-	root.appendChild(card)
+		root.innerHTML = ""
+    	res.results.map(el => {
+			const card = document.createElement('div')
+			const title = document.createElement('a')
+			title.href = "#"+el.id;
+			title.innerText = el.title
+	   		const br = document.createElement('br')
+	   		const img = document.createElement('img')
+   			img.src = 'http://image.tmdb.org/t/p/w200' + el.poster_path
+   			card.appendChild(img)
+			card.appendChild(br)
+			card.appendChild(title)
+			root.appendChild(card)
+       })
     })
-})
-}
-
-function CreateFilmCard(el)
-{
-	const card = document.createElement('div')
-	const title = document.createElement('p1')
-	hash= el.id;
-	title.innerText = el.title
-   	const br = document.createElement('br')
-   	const img = document.createElement('img')
-   	img.src = 'http://image.tmdb.org/t/p/w200' + el.poster_path
-   	card.appendChild(img)
-	card.appendChild(br)
-	card.appendChild(title)
-	card.addEventListener('click', ()=>ChangeHash(hash))
-	return card
 }
 
 function ChangeHash(hash)
@@ -35,41 +26,49 @@ function ChangeHash(hash)
 
 function getRouteInfo()
 {
-
 	const hash = location.hash.slice(1);
 	return hash
 }
 
-function GetMovie(id) 
+function GetMovie(root, id) 
 {
+	root.innerHTML = ""
 	url='https://api.themoviedb.org/3/movie/'+id+'?api_key=3a66dca4ab982982a2e4d01db94410b4&language=uk';
-	console.log(url);
 	fetch(url)
 		.then(response=>response.json())
 		.then(res=>{
-			const root = document.createElement('div')
-    		document.body.appendChild(root)
 			const title = document.createElement('h1')
 			title.innerText = res.title
-
+			const about = document.createElement('p1')
+			about.innerText = "Опис:  "+ res.overview
+			let a = "Жанри: "
+			const genres = document.createElement('p1')
+			res.genres.map (g =>
+				{
+					a+=g.name +"  "
+				}) 
+			genres.innerText =a
     		const br = document.createElement('br')
-      
     		const img = document.createElement('img')
 			img.src = 'http://image.tmdb.org/t/p/w500' + res.poster_path
-    
 			root.appendChild(title)
 			root.appendChild(img)
-
+			root.appendChild(br)
+			root.appendChild(about)
+			root.appendChild(br)
+			root.appendChild(genres)
 			
 		})
 }
 
 function Control() 
 {
-	const id = getRouteInfo();	
-	if (id==="start") GetTrendFilms()
-	else GetMovie(id)
+	    const root = document.getElementById('result')
+		const id = getRouteInfo()
+		if (id==="start") GetTrendFilms(root)
+		else GetMovie(root, id)
 }
-window.addEventListener('hashchange', console.log("OK"))
-location.hash = "#start";
-Control();
+if (location.hash ==="") location.hash = "#start"
+addEventListener(window.onhashchange, Control())
+
+
